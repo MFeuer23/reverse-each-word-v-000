9225da60ebe31960935617a743842b4dd4e45faf
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-08 18:52:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   backwards_array = []
   array = string.split(" ")
-  array.map do |word|
+  array.collect do |word|
     backwards_array << word.reverse
   end
   backwards_array.join(" ")
